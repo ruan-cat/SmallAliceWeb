8e5cb8369d8f3fd544253ff592414dfc501c5328
ruan-cat <1219043956@qq.com>
2023-03-06 12:56:23 +0800

    🤔 save-file: 初步删减内容，准备部署；

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,19 +1,14 @@
-import { defineUserConfig } from "vuepress";
-import theme from "./theme.js";
+import { defineUserConfig } from 'vuepress';
+import theme from './theme.js';
 
 export default defineUserConfig({
-  base: "/",
+  base: '/',
 
   locales: {
-    "/": {
-      lang: "en-US",
-      title: "Docs Demo",
-      description: "A docs demo for vuepress-theme-hope",
-    },
-    "/zh/": {
-      lang: "zh-CN",
-      title: "文档演示",
-      description: "vuepress-theme-hope 的文档演示",
+    '/': {
+      lang: 'en-US',
+      title: 'Docs Demo',
+      description: 'A docs demo for vuepress-theme-hope',
     },
   },
 
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,72 +1,53 @@
-import { hopeTheme } from "vuepress-theme-hope";
-import { enNavbar, zhNavbar } from "./navbar/index.js";
-import { enSidebar, zhSidebar } from "./sidebar/index.js";
+import { hopeTheme } from 'vuepress-theme-hope';
+import { enNavbar, zhNavbar } from './navbar/index.js';
+import { enSidebar, zhSidebar } from './sidebar/index.js';
 
 export default hopeTheme({
-  hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
+  hostname: 'https://vuepress-theme-hope-docs-demo.netlify.app',
 
   author: {
-    name: "Mr.Hope",
-    url: "https://mrhope.site",
+    name: 'Mr.Hope',
+    url: 'https://mrhope.site',
   },
 
-  iconAssets: "iconfont",
+  iconAssets: 'iconfont',
 
-  logo: "/logo.svg",
+  logo: '/logo.svg',
 
-  repo: "vuepress-theme-hope/vuepress-theme-hope",
+  repo: 'vuepress-theme-hope/vuepress-theme-hope',
 
-  docsDir: "demo/theme-docs/src",
+  docsDir: 'src',
 
   locales: {
-    "/": {
+    '/': {
       // navbar
       navbar: enNavbar,
 
       // sidebar
-      sidebar: enSidebar,
+      // sidebar: enSidebar,
+      sidebar: 'structure',
 
-      footer: "Default footer",
+      footer: 'Default footer',
 
       displayFooter: true,
 
       metaLocales: {
-        editLink: "Edit this page on GitHub",
-      },
-    },
-
-    /**
-     * Chinese locale config
-     */
-    "/zh/": {
-      // navbar
-      navbar: zhNavbar,
-
-      // sidebar
-      sidebar: zhSidebar,
-
-      footer: "默认页脚",
-
-      displayFooter: true,
-
-      // page meta
-      metaLocales: {
-        editLink: "在 GitHub 上编辑此页",
+        editLink: 'Edit this page on GitHub',
       },
     },
   },
 
   encrypt: {
     config: {
-      "/demo/encrypt.html": ["1234"],
-      "/zh/demo/encrypt.html": ["1234"],
+      '/demo/encrypt.html': ['1234'],
+      '/zh/demo/encrypt.html': ['1234'],
     },
   },
 
   plugins: {
     comment: {
       // @ts-expect-error: You should generate and use your own comment service
-      provider: "Waline",
+      provider: 'Waline',
     },
 
     // all features are enabled for demo, only preserve features you need here
@@ -88,20 +69,20 @@ export default hopeTheme({
       mark: true,
       mermaid: true,
       playground: {
-        presets: ["ts", "vue"],
+        presets: ['ts', 'vue'],
       },
       presentation: {
-        plugins: ["highlight", "math", "search", "notes", "zoom"],
+        plugins: ['highlight', 'math', 'search', 'notes', 'zoom'],
       },
       stylize: [
         {
-          matcher: "Recommended",
+          matcher: 'Recommended',
           replacer: ({ tag }) => {
-            if (tag === "em")
+            if (tag === 'em')
               return {
-                tag: "Badge",
-                attrs: { type: "tip" },
-                content: "Recommended",
+                tag: 'Badge',
+                attrs: { type: 'tip' },
+                content: 'Recommended',
               };
           },
         },
