4eb88391651a0de2d1def863c1716bd4141008b3
ruan-cat <1219043956@qq.com>
2023-07-27 09:36:39 +0800

    ⚙️ config: 增加顶部导航栏信息；

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -1,14 +1,19 @@
-import { navbar } from 'vuepress-theme-hope';
+import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
   {
-    text: '版本发行记录',
-    prefix: '/releases-record/',
+    text: "版本发行记录",
+    prefix: "/releases-record/",
     children: [
       {
-        text: 'v3.20 喵喵呜',
-        link: '3.20/',
+        text: "v3.20 喵喵呜",
+        link: "3.20/",
       },
     ],
+  },
+  {
+    text: "QQ群群规",
+    prefix: "/qq-group/",
+    children: [{ text: "群条款", link: "clause/" }],
   },
 ]);
