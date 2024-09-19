1585f712ce89b2a689e5e9e05360e8e34e0a0279
ruan-cat <1219043956@qq.com>
2024-09-19 16:44:15 +0800

    ⚙️ config: 配置 vercel-deploy-tool ；

---
+++ .config/vercel-deploy-tool.ts
@@ -0,0 +1,22 @@
+import { type Config } from "@ruan-cat/vercel-deploy-tool/config.ts";
+
+const config: Config = {
+	vercelProjetName: "small-alice-web-odse",
+	vercelOrgId: "team_cUeGw4TtOCLp0bbuH8kA7BYH",
+	vercelProjectId: "prj_vdrAvRthiSjkhotfPTXFSV5e1KQW",
+	vercelToken: "",
+
+	deployTargets: [
+		{
+			type: "userCommands",
+			outputDirectory: "docs/.vuepress/dist/**/*",
+			targetCWD: "/",
+			url: [
+				"small-alice-web-dev.ruancat6312.top",
+				"small-alice-web.ruan-cat.com",
+			],
+		},
+	],
+};
+
+export default config;
