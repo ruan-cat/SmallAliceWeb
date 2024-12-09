a0d1a16895ca24a4d56990a1f97bfcb907554d49
ruan-cat <1219043956@qq.com>
2024-12-10 02:44:20 +0800

    perf: 使用域名配置，集中配置域名。

--- .config/vercel-deploy-tool.ts
+++ .config/vercel-deploy-tool.ts
@@ -1,4 +1,5 @@
 import { type Config } from "@ruan-cat/vercel-deploy-tool/src/config.ts";
+import { domains } from "@ruan-cat/domains";
 
 const config: Config = {
 	vercelProjetName: "small-alice-web-odse",
@@ -11,12 +12,7 @@ const config: Config = {
 			type: "userCommands",
 			outputDirectory: "docs/.vuepress/dist",
 			targetCWD: "./",
-			url: [
-				"small-alice-web.ruancat6312.top",
-				"small-alice-web.ruan-cat.com",
-				"drill.ruancat6312.top",
-				"drill.ruan-cat.com",
-			],
+			url: domains["drill-doc"] as unknown as string[],
 			userCommands: ["pnpm -C=./ docs:build"],
 		},
 	],
