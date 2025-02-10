d91ff3d27cce147837957ac2982ac0936de3ee87
ruan-cat <1219043956@qq.com>
2025-02-10 17:46:03 +0800

    🦄 refactor: 改掉错误的包封装方式。

--- scripts/docx2md/index.ts
+++ scripts/docx2md/index.ts
@@ -83,8 +83,12 @@ async function cloneDrillDocxRepo() {
 
 /**
  * 用 git clone 命令克隆钻头的docx文档仓库
+ * @description
+ * 1. 用 git clone --depth=1 https://github.com/ruan-cat/drill-docx 命令，实现克隆项目
+ * 2. 克隆到 catalog.drillDocx 目录内
+ * 3. 用 generateSpawn 函数包装运行命令
  */
-function cloneDrillDocxRepoWithGit() {
+function cloneDrillDocxRepoWithGitTask() {
 	const command = "git";
 	const parameters = [
 		"clone",
@@ -98,17 +102,6 @@ function cloneDrillDocxRepoWithGit() {
 /** 准备dist目录任务 */
 function prepareDistTask() {
 	return generateSimpleAsyncTask(prepareDist);
-}
-
-/**
- * 克隆钻头的docx文档仓库任务
- * @description
- * 1. 用 git clone --depth=1 https://github.com/ruan-cat/drill-docx 命令，实现克隆项目
- * 2. 克隆到 catalog.drillDocx 目录内
- * 3. 用 generateSpawn 函数包装运行命令
- */
-function cloneDrillDocxRepoWithGitTask() {
-	return generateSimpleAsyncTask(cloneDrillDocxRepoWithGit);
 }
 
 /**
